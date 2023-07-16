21c76f4d7ead74f29bbd6c6ef75edcea16323529
Ayush Praveen <ayushpr.ap@outlook.com>
2023-07-16 13:16:24 +0530

    working webpage

---
+++ pages/api/generate.js
@@ -0,0 +1,42 @@
+const bufferToBase64 = (buffer) => {
+    let arr = new Uint8Array(buffer);
+    const base64 = btoa(
+        arr.reduce((data, byte) => data + String.fromCharCode(byte), '')
+    );
+    return `data:image/png;base64,${base64}`;
+}
+
+const generate = async (req, res) => {
+    console.log('request received');
+
+    const input = JSON.parse(req.body).input;
+
+    const response = await fetch(
+        `https://api-inference.huggingface.co/models/ayush-praveen/sd-1-5-ayush`,
+        {
+            headers: {
+                Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
+                'Content-Type': 'application/json',
+                'x-use-cache': 'false'
+            },
+            method: 'POST',
+            body: JSON.stringify({inputs: input}),
+        }
+    )
+
+    if (response.ok) {
+        const buffer = await response.arrayBuffer();
+        const base64 = bufferToBase64(buffer);
+
+        res.status(200).json({image: base64})
+    } else if (response.status === 503) {
+        const json = await response.json();
+        res.status(503).json(json);
+    } else {
+        const json = await response.json();
+        res.status(response.status).json({ error: response.statusText });
+    }
+
+}
+
+export default generate;
